4a23713c8af39fb466f890b4e8599e890cc6c3ed
giasudin <giasudin.me90@gmail.com>
2025-11-05 13:24:13 +0600

    add deployment file

---
+++ production_start_project.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo Stop  Application
+
+kill -TERM $(cat mini_erp.pid)
+
+sleep 1s
+ps ax|grep gunicorn
+echo Stop minierp Gunicorn
+
+echo Starting Gunicorn and active virtualenv
+# virtualenv for production.
+source .venv/bin/activate
+#source /home/ubuntu/PycharmProjects/djangoGunicorn/bin/activate
+
+gunicorn -c server_deploy.py
+sleep 1s
+ps ax|grep gunicorn
+
+#Deactivate Virtual env for production
+
+deactivate
+echo virtualenv deactivate
---
+++ server_deploy.py
@@ -0,0 +1,20 @@
+"""Gunicorn *development* config file"""
+
+# Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
+wsgi_app = "core.wsgi:application"
+# The granularity of Error log outputs
+loglevel = "debug"
+# The number of worker processes for handling requests
+# workers = 3
+# The socket to bind
+bind = "0.0.0.0:9090"
+# Restart workers when code changes (development only!)
+# reload = True
+# Write access and error info to /var/log
+# errorlog = "/var/log/gunicorn/mini_erp.log"
+# Redirect stdout/stderr to log file
+capture_output = True
+# PID file so you can easily fetch process ID
+pidfile = "mini_erp.pid"
+# Daemonize the Gunicorn process (detach & enter background)
+daemon = False
